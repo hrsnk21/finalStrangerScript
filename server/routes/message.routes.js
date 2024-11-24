357c60659cc3bb46fcc60f38da7f6a07fbf205af
hrsnk21 <155194869+hrsnk21@users.noreply.github.com>
2024-11-24 18:39:55 +0530

    project completed

---
+++ server/routes/message.routes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const protectRoute  =  require("../middleware/auth.middleware.js");
+const { getMessages, getUsersForSidebar, sendMessage } = require("../controller/message.controller.js")
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const authRoutes =  require('./routes/auth.routes'); 
+const messageRoutes = require('./routes/message.routes')
+const connectToDb = require('./lib/db')
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const{server,app} = require('./lib/socket')
+
+const path = require('path')
+
+dotenv.config()
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+      methods:['GET','POST','PUT','UPDATE','DELETE']
+    })
+  );
+
+app.use('/api/v1/auth',authRoutes)
+app.use("/api/v1/messages", messageRoutes);
+
+
+const PORT = process.env.PORT
+// let __dirname = path.resolve()
+
+if(process.env.NODE_ENV = 'production'){
+  app.use(express.static(path.join(__dirname,"./client/dist")))
+  app.get("*",(req,res)=>{
+      res.sendFile(path.join(__dirname,"./client","dist","index.html"))
+  })
+}
+
+
+server.listen(PORT,()=>{
+    console.log(`server is live at port ${PORT}`)
+    connectToDb();
+})
